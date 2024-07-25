7bd367c65b7e370741c03bc5c9e0beccffbafdd9
krolikbrunatny <rabbit@makemethink.net>
2024-07-25 20:22:06 +0200

    obsidian frontmatter and new notes location

--- dotfiles/nvim/lua/plugins/obsidian.lua
+++ dotfiles/nvim/lua/plugins/obsidian.lua
@@ -21,5 +21,20 @@ return {
       default_tags = { "daily-notes" },
       template = nil,
     },
+    note_frontmatter_func = function(note)
+      local out = { id = note.id, tags = note.tags }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
+    notes_subdir = "zettel",
+    new_notes_location = "notes_subdir",
   },
 }
