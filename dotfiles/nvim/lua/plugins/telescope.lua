7da029f7e2574c190bb7c6cc6b19ebd9a89ad826
krolikbrunatny <rabbit@makemethink.net>
2025-01-16 16:54:20 +0100

    search in hidden files with telescope live grep

---
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+  require("telescope").setup({
+    pickers = {
+      live_grep = {
+        additional_args = function(opts)
+          return { "--hidden" }
+        end,
+      },
+    },
+  }),
+}
